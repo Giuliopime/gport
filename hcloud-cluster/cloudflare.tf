ff43217bb987383ca6354a23353a604ee74decee
Giuliopime <giuliopime@gmail.com>
2026-02-26 00:13:34 +0100

    feat: cloudflare resources + longhorn

--- hcloud-cluster/cloudflare.tf
+++ hcloud-cluster/cloudflare.tf
@@ -1,14 +1,14 @@
-resource "cloudflare_dns_record" "public_api_kube_dns_record" {
+resource "cloudflare_dns_record" "kube_public_api" {
   zone_id = var.cloudflare_zone_id
-  name = "public.api.kube"
-  ttl = 1
-  type = "A"
+  name    = "public.api.kube"
+  ttl     = 1
+  type    = "A"
   content = module.kube-hetzner.lb_control_plane_ipv4
   # makes it so connection go directly to the cluster without passing through Cloudflare
   proxied = false
 }
 
-resource "cloudflare_dns_record" "grafana_dns_record" {
+resource "cloudflare_dns_record" "grafana" {
   zone_id = var.cloudflare_zone_id
   name    = "grafana"
   type    = "A"
@@ -17,8 +17,17 @@ resource "cloudflare_dns_record" "grafana_dns_record" {
   content = module.kube-hetzner.ingress_public_ipv4
 }
 
+resource "cloudflare_dns_record" "longhorn" {
+  zone_id = var.cloudflare_zone_id
+  name    = "longhorn"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+  content = module.kube-hetzner.ingress_public_ipv4
+}
+
 # argoCD
-resource "cloudflare_dns_record" "argocd_grpc_dns_record" {
+resource "cloudflare_dns_record" "argocd_grpc" {
   zone_id = var.cloudflare_zone_id
   name    = "grpc.argocd"
   type    = "A"
@@ -27,7 +36,7 @@ resource "cloudflare_dns_record" "argocd_grpc_dns_record" {
   content = module.kube-hetzner.ingress_public_ipv4
 }
 
-resource "cloudflare_dns_record" "argocd_dns_record" {
+resource "cloudflare_dns_record" "argocd" {
   zone_id = var.cloudflare_zone_id
   name    = "argocd"
   type    = "A"
